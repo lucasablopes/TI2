644fe632630eb8173551411ae43a19c8e08c233d
Lucas Abreu <89923438+lucasablopes@users.noreply.github.com>
2022-08-25 20:42:54 -0300

    Add files via upload

---
+++ EX02/maven.demo/src/main/java/Carro.java
@@ -0,0 +1,63 @@
+public class Carro 
+{
+    private int id;
+    private String modelo;
+    private int ano;
+    
+
+    public Carro()
+    {
+        id = -1;
+        modelo = "";
+        ano = -1;
+    }
+    public Carro(int id, String modelo, int ano) 
+    {
+        this.id = id;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+
+    public int getId() 
+    {
+        return id;
+    }
+
+
+    public void setId(int id) 
+    {
+        this.id = id;
+    }
+
+
+    public String getModelo() 
+    {
+        return modelo;
+    }
+
+
+    public void setModelo(String modelo) 
+    {
+        this.modelo = modelo;
+    }
+
+
+    public int getAno() 
+    {
+        return ano;
+    }
+
+
+    public void setAno(int ano) {
+
+        this.ano = ano;
+    }
+
+    @Override
+	public String toString() 
+    {
+		return "Usuario [id=" + id + ",modelo=" +modelo + ", ano=" + ano + "]";
+	}
+    
+}
